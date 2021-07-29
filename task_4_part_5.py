e013a61c3fa18b25a188f95daa05c4cc5d16f2a2
v1adimir_k <v1adimir_k@mail.ru>
2021-07-29 22:44:11 +0300

    Не заметил, что в условии требуются только четные числа

--- task_4_part_5.py
+++ task_4_part_5.py
@@ -6,7 +6,7 @@
 """
 from functools import reduce
 
-my_list = [x for x in range(100, 1001)]  # == list(range(100, 1001))
+my_list = [x for x in range(100, 1001, 2)]
 result = reduce(lambda x, y: x*y, my_list)
 
 print(my_list)
